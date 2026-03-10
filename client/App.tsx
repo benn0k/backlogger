1083e92390efb71b4005489be7619a6accc82209
benn0k <rbennek.rb@gmail.com>
2026-03-10 11:03:20 -0500

    Mockup Card idea, add Fonts, start styles
    Mostly hardcoded, next step is to break out card into individual
    components

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,31 @@
 import "./App.css";
+import Genre from "./components/ui/Card/Genre";
+import Status from "./components/ui/Card/Status";
 
 function App() {
   return (
     <>
-      {" "}
-      <div>
-        <h1>Hello World</h1>
+      <div className="card-container outline outline-dashed outline-offset-15 ">
+        <h1 className="title">Marathon</h1>
+        <div className="description">
+          NULL_POINTER_EXCEPTION (good luck, runner :)){" "}
+        </div>
+        <div className="notes">
+          I met a traveller from an antique land, Who said—“Two vast and
+          trunkless legs of stone Stand in the desert. . . . Near them, on the
+          sand, Half sunk a shattered visage lies, whose frown, And wrinkled
+          lip, and sneer of cold command, Tell that its sculptor well those
+          passions read Which yet survive, stamped on these lifeless things, The
+          hand that mocked them, and the heart that fed; And on the pedestal,
+          these words appear: My name is Ozymandias, King of Kings; Look on my
+          Works, ye Mighty, and despair!" Nothing beside remains. Round the
+          decay Of that colossal Wreck, boundless and bare The lone and level
+          sands stretch far away.
+        </div>
+        <div className="card-bottom my-2 flex flex-row justify-between">
+          <Status></Status>
+          <Genre></Genre>
+        </div>
       </div>
     </>
   );
---
+++ client/components/ui/Card/Genre.tsx
@@ -0,0 +1,7 @@
+function Genre() {
+  return (
+    <div className="font-[vt323] text-gray-500  2 w-fit px-2 ">Shooter</div>
+  );
+}
+
+export default Genre;
